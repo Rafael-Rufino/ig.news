c894757b7d3ad9cd9e7946b54497b1d73b5b4ecc
rafaelrufino <rafaelrufino.52@gmail.com>
2021-07-10 17:03:49 -0300

    01 - estrutura da aplicação

---
+++ src/pages/index.tsx
@@ -0,0 +1,15 @@
+
+import Head from 'next/head';
+export default function Home() {
+  return (
+    <>
+        <Head>
+            <title>Home | ig.news</title>
+        </Head>
+        <div>
+       
+          <h1>Hello Word</h1>
+        </div>
+    </>
+  )
+}
