137f6f02b3ffdf113704b0fde3090feb38649808
rafaelrufino <rafaelrufino.52@gmail.com>
2021-07-13 12:39:31 -0300

    02 - Componentes e páginas

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,9 +2,16 @@
 import  '../styles/global.scss';
 
 import {AppProps} from 'next/app'
+import React from 'react';
+import { Header } from '../components/Header';
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return(
+  <>
+    <Header/>
+    <Component {...pageProps} />
+  </>
+  )
 }
 
 export default MyApp
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,15 +1,28 @@
+import styles from './home.module.scss';
+
 
 import Head from 'next/head';
+import { SubscriberButton } from '../components/SubscribeButton';
 export default function Home() {
   return (
     <>
         <Head>
             <title>Home | ig.news</title>
         </Head>
-        <div>
-       
-          <h1>Hello Word</h1>
-        </div>
+        <main className={styles.contentContainer}>
+          <section className={styles.hero}>
+            <span>👏 Hey, welcome</span>
+            <h1>News abount the <span>React</span> world.</h1>
+            <p>
+              Get access to all the publications <br/>
+              <span>for $9.90 month</span>
+            </p>
+            <SubscriberButton/>
+
+          </section>
+            <img src="/images/avatar.svg" alt="Girl coding" />
+        
+        </main>
     </>
   )
 }
