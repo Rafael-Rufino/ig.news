c82d60e46afd2401490779c4fda5a3f049f9388e
RafaelRufino <rafaelrufino.52@gmail.com>
2021-08-11 18:33:45 -0300

    01 - Trabalhando com CMS

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,6 +1,10 @@
 import React from 'react';
+
 import { SignInButton } from '../SignInButton';
 import styles from './styles.module.scss';
+import { ActiveLink } from '../ActiveLink';
+
+
 export function Header(){
   return(
     <header className={styles.headerContainer}>
@@ -8,8 +12,14 @@ export function Header(){
          {/* eslint-disable-next-line @next/next/no-img-element */}
         <img src="/images/logo.svg" alt="ig.news"/>
         <nav>
-            <a className={styles.active}>Home</a>
-            <a>Posts</a>
+          <ActiveLink activeClassName={styles.active} href="/" prefetch>
+              <a>Home</a>
+          </ActiveLink>
+
+          <ActiveLink activeClassName={styles.active} href="/posts" prefetch>
+              <a >Posts</a>
+          </ActiveLink>
+
         </nav>
         <SignInButton/>
       </div>
